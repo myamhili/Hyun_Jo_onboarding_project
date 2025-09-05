5a6e805ebc91a993512b7d0032dc7e4b11928e57
myamhili <userhyun0629@gmail.com>
2025-09-05 15:59:10 -0400

    test

--- test/tb.v
+++ test/tb.v
@@ -25,10 +25,10 @@ module tb ();
 `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
-`endif
+`endif// test
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_uwasic_onboarding_Hyun_Jo user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
